904c681d79bd33c7062317d2fd1d01cebc12953c
vayrnn <125257278+vayrnn@users.noreply.github.com>
2025-04-02 17:51:38 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Вадим: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!");
+    }
+}
